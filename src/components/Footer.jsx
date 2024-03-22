b665859f90ac79e27363911fc2e8088d8b138c3d
Dayananda Bindhani <115238557+PROFFARO@users.noreply.github.com>
2024-03-22 21:11:39 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import portfolioDetails from "../constants/store";
 
 const footer = () => {
   return (
-    <section className="mt-1 bg-transparent flex flex-row justify-center text-xl font-mono font-medium">
+    <section className="mt-1 bg-transparent flex flex-row justify-center text-sm font-mono font-medium lg:text-xl">
       <div className="text-white">
         &lt; Created with 💌 by {portfolioDetails.name} / &gt;
       </div>
